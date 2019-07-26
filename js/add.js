bca222d67be0492adff153c1a0b95fb763b5e614
unknown <ksharper@studentmba.org>
2019-07-26 18:02:48 -0400

    SGW Lips demo

---
+++ js/add.js
@@ -0,0 +1,15 @@
+const product = React.createElement(
+    "div",
+    { class : "row",},
+    <div class="col-md-4"><h3>Lip Gloss</h3>
+                    
+                <img class="products-7"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkYx3pzFWrmOtmI0UFvYKnUonZNZPN_22nYuR6f7WtwZHJKhT1Vw">
+            
+                <div class="button-7">
+                   <button>Hello</button>
+                   
+                </div>
+                <p>$4.99</p>
+            </div>
+)
---
+++ js/index.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $(".products-1").hover(function(){
+        $(".button-1").toggle();
+        $(".overlay").toggle();
+    
+    })
+    $(".products-2").hover(function(){
+        $(".button-2").toggle();
+        $(".overlay").toggle();
+    
+    })
+    $(".products-3").hover(function(){
+        $(".button-3").toggle();
+        $(".overlay").toggle();
+    
+    })
+    $(".products-4").hover(function(){
+        $(".button-4").toggle();
+        $(".overlay").toggle();
+    
+    })
+    $(".products-5").hover(function(){
+        $(".button-5").toggle();
+        $(".overlay").toggle();
+    
+    })
+    $(".products-6").hover(function(){
+        $(".button-6").toggle();
+        $(".overlay").toggle();
+    
+    })
+    $(".products-7").hover(function(){
+        $(".button-7").toggle();
+        $(".overlay").toggle();
+    
+    })
+    $(".products-8").hover(function(){
+        $(".button-8").toggle();
+        $(".overlay").toggle();
+    
+    })
+    $(".products-9").hover(function(){
+        $(".button-9").toggle();
+        $(".overlay").toggle();
+    
+    })
+let products = document.querySelectorAll('.products')
+let buttons = document.querySelectorAll("button")
+
+ 
+})
